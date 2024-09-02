d72d046215f0f5283855b0d18f2f5bd8361d8db8
Anuj Pal <palanuj247@gmail.com>
2024-09-02 13:54:33 +0530

    add toggling functionality

--- project/assets/scripts/app.js
+++ project/assets/scripts/app.js
@@ -1,2 +1,25 @@
+const addMovieModal = document.getElementById('add-modal');
+const stratAddMovieButton = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible');
+};
+
+const toggleMovieModal = () => {
+    addMovieModal.classList.toggle('visible');
+    toggleBackdrop();
+};
+const toggleBackdropHandler = () => {
+    toggleMovieModal();
+};
+
+const cancelAddMovie = () => {
+    toggleMovieModal();
+};
+
+stratAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', toggleBackdropHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
 
